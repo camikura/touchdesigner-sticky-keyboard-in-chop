aea908beaa0d01ddc385cceaa58ac177170536f7
camikura <akira.kamikura@gmail.com>
2022-01-14 11:36:23 +0900

    Convert hard tabs to 2 spaces

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -13,18 +13,18 @@ using namespace std;
 
 Parameters::Parameters()
 {
-	for (char c = '0'; c <= '9'; c++)
-	{
-		string str(1, c);
-		enable_keys.push_back(str);
-	}
-	for (char c = 'a'; c <= 'z'; c++)
-	{
-		string str(1, c);
-		enable_keys.push_back(str);
-	}
+  for (char c = '0'; c <= '9'; c++)
+  {
+    string str(1, c);
+    enable_keys.push_back(str);
+  }
+  for (char c = 'a'; c <= 'z'; c++)
+  {
+    string str(1, c);
+    enable_keys.push_back(str);
+  }
 
-	modifier = 0;
+  modifier = 0;
 }
 
 Parameters::~Parameters()
@@ -33,51 +33,51 @@ Parameters::~Parameters()
 
 vector<string> Parameters::split(string str, string del = " ")
 {
-	vector<string> result;
-	size_t start = 0;
-	size_t end = str.find(del);
-	while (end != -1)
-	{
-		result.push_back(str.substr(start, end - start));
-		start = end + del.size();
-		end = str.find(del, start);
-	}
-	result.push_back(str.substr(start, end - start));
-	return result;
+  vector<string> result;
+  size_t start = 0;
+  size_t end = str.find(del);
+  while (end != -1)
+  {
+    result.push_back(str.substr(start, end - start));
+    start = end + del.size();
+    end = str.find(del, start);
+  }
+  result.push_back(str.substr(start, end - start));
+  return result;
 }
 
 vector<string> Parameters::evalKeys(const OP_Inputs *input)
 {
-	string str = input->getParString(KeysName);
+  string str = input->getParString(KeysName);
 
-	vector<string> keys;
-	for (auto k : split(str))
-	{
-		if (k == "keycomma")
-			keys.push_back("comma");
-		else if (k == "keyperiod")
-			keys.push_back("period");
-		else
-		{
-			for (auto ek : enable_keys)
-			{
-				if (k == ek)
-					keys.push_back(k);
-			}
-		}
-	}
+  vector<string> keys;
+  for (auto k : split(str))
+  {
+    if (k == "keycomma")
+      keys.push_back("comma");
+    else if (k == "keyperiod")
+      keys.push_back("period");
+    else
+    {
+      for (auto ek : enable_keys)
+      {
+        if (k == ek)
+          keys.push_back(k);
+      }
+    }
+  }
 
-	return keys;
+  return keys;
 }
 
 bool Parameters::evalActive(const OP_Inputs *input)
 {
-	return input->getParInt(ActiveName) == 0;
+  return input->getParInt(ActiveName) == 0;
 }
 
 int Parameters::evalModifier(const OP_Inputs *input)
 {
-	return input->getParInt(ModifierName);
+  return input->getParInt(ModifierName);
 }
 
 #pragma endregion
@@ -86,44 +86,44 @@ int Parameters::evalModifier(const OP_Inputs *input)
 
 void Parameters::setup(OP_ParameterManager *manager)
 {
-	{
-		const char *names[] = {"On", "Off"};
-		const char *labels[] = {"On", "Off"};
+  {
+    const char *names[] = {"On", "Off"};
+    const char *labels[] = {"On", "Off"};
 
-		OP_StringParameter p;
-		p.name = ActiveName;
-		p.label = ActiveLabel;
-		p.page = "Keys";
-		p.defaultValue = names[0];
-		OP_ParAppendResult res = manager->appendMenu(p, 2, names, labels);
+    OP_StringParameter p;
+    p.name = ActiveName;
+    p.label = ActiveLabel;
+    p.page = "Keys";
+    p.defaultValue = names[0];
+    OP_ParAppendResult res = manager->appendMenu(p, 2, names, labels);
 
-		assert(res == OP_ParAppendResult::Success);
-	}
-	{
-		OP_StringParameter p;
-		p.name = KeysName;
-		p.label = KeysLabel;
-		p.page = "Keys";
-		OP_ParAppendResult res = manager->appendString(p);
-		assert(res == OP_ParAppendResult::Success);
-	}
-	{
-		const char *names[] = {
-				"Ignore", "None", "Control", "Alt", "Controlandalt",
-				"Shift", "Shiftandalt", "Shiftandcontrol", "Shiftandcontrolandalt"};
-		const char *labels[] = {
-				"Ignore", "None", "Control", "Alt", "Control and Alt",
-				"Shift", "Shift and Alt", "Shift and Control", "Shift and Control and Alt"};
+    assert(res == OP_ParAppendResult::Success);
+  }
+  {
+    OP_StringParameter p;
+    p.name = KeysName;
+    p.label = KeysLabel;
+    p.page = "Keys";
+    OP_ParAppendResult res = manager->appendString(p);
+    assert(res == OP_ParAppendResult::Success);
+  }
+  {
+    const char *names[] = {
+        "Ignore", "None", "Control", "Alt", "Controlandalt",
+        "Shift", "Shiftandalt", "Shiftandcontrol", "Shiftandcontrolandalt"};
+    const char *labels[] = {
+        "Ignore", "None", "Control", "Alt", "Control and Alt",
+        "Shift", "Shift and Alt", "Shift and Control", "Shift and Control and Alt"};
 
-		OP_StringParameter p;
-		p.name = ModifierName;
-		p.label = ModifierLabel;
-		p.page = "Keys";
-		p.defaultValue = names[0];
-		OP_ParAppendResult res = manager->appendMenu(p, 9, names, labels);
+    OP_StringParameter p;
+    p.name = ModifierName;
+    p.label = ModifierLabel;
+    p.page = "Keys";
+    p.defaultValue = names[0];
+    OP_ParAppendResult res = manager->appendMenu(p, 9, names, labels);
 
-		assert(res == OP_ParAppendResult::Success);
-	}
+    assert(res == OP_ParAppendResult::Success);
+  }
 }
 
 #pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -27,157 +27,157 @@ using namespace std;
 class StickyKeyboardInCHOP : public CHOP_CPlusPlusBase
 {
 private:
-	vector<string> keydown_keys;
-	bool active;
-	int modifier;
+  vector<string> keydown_keys;
+  bool active;
+  int modifier;
 
-	bool testModifierKey();
-	bool getKeyPressed(string);
+  bool testModifierKey();
+  bool getKeyPressed(string);
 
-	Parameters params;
+  Parameters params;
 
 public:
-	StickyKeyboardInCHOP(const OP_NodeInfo *info)
-	{
-		active = false;
-		modifier = 0;
-	}
+  StickyKeyboardInCHOP(const OP_NodeInfo *info)
+  {
+    active = false;
+    modifier = 0;
+  }
 
-	virtual ~StickyKeyboardInCHOP()
-	{
-	}
+  virtual ~StickyKeyboardInCHOP()
+  {
+  }
 
-	void getGeneralInfo(CHOP_GeneralInfo *ginfo, const OP_Inputs *inputs, void *reserved1)
-	{
-		ginfo->cookEveryFrameIfAsked = true;
-		ginfo->timeslice = false;
-		ginfo->inputMatchIndex = 0;
-	}
+  void getGeneralInfo(CHOP_GeneralInfo *ginfo, const OP_Inputs *inputs, void *reserved1)
+  {
+    ginfo->cookEveryFrameIfAsked = true;
+    ginfo->timeslice = false;
+    ginfo->inputMatchIndex = 0;
+  }
 
-	bool getOutputInfo(CHOP_OutputInfo *info, const OP_Inputs *inputs, void *reserved1)
-	{
-		keydown_keys = params.evalKeys(inputs);
-		active = params.evalActive(inputs);
-		modifier = params.evalModifier(inputs);
+  bool getOutputInfo(CHOP_OutputInfo *info, const OP_Inputs *inputs, void *reserved1)
+  {
+    keydown_keys = params.evalKeys(inputs);
+    active = params.evalActive(inputs);
+    modifier = params.evalModifier(inputs);
 
-		info->numSamples = 1;
-		info->numChannels = (int32_t)keydown_keys.size();
-		return true;
-	}
+    info->numSamples = 1;
+    info->numChannels = (int32_t)keydown_keys.size();
+    return true;
+  }
 
-	void getChannelName(int32_t index, OP_String *name, const OP_Inputs *inputs, void *reserved1)
-	{
-		name->setString(('k' + keydown_keys[index]).c_str());
-	}
+  void getChannelName(int32_t index, OP_String *name, const OP_Inputs *inputs, void *reserved1)
+  {
+    name->setString(('k' + keydown_keys[index]).c_str());
+  }
 
-	void execute(CHOP_Output *output, const OP_Inputs *inputs, void *reserved)
-	{
-		for (int i = 0; i < output->numChannels; ++i)
-		{
-			string key = keydown_keys[i];
-			output->channels[i][0] = getKeyPressed(key);
-		}
-	}
+  void execute(CHOP_Output *output, const OP_Inputs *inputs, void *reserved)
+  {
+    for (int i = 0; i < output->numChannels; ++i)
+    {
+      string key = keydown_keys[i];
+      output->channels[i][0] = getKeyPressed(key);
+    }
+  }
 
-	void setupParameters(OP_ParameterManager *manager, void *reserved1)
-	{
-		params.setup(manager);
-	}
+  void setupParameters(OP_ParameterManager *manager, void *reserved1)
+  {
+    params.setup(manager);
+  }
 };
 
 bool StickyKeyboardInCHOP::testModifierKey()
 {
-	// None
-	if (modifier == 1)
-		return !(GetAsyncKeyState(VK_SHIFT) & 0x8000) && !(GetAsyncKeyState(VK_CONTROL) & 0x8000) && !(GetAsyncKeyState(VK_MENU) & 0x8000);
+  // None
+  if (modifier == 1)
+    return !(GetAsyncKeyState(VK_SHIFT) & 0x8000) && !(GetAsyncKeyState(VK_CONTROL) & 0x8000) && !(GetAsyncKeyState(VK_MENU) & 0x8000);
 
-	// Control
-	if (modifier == 2)
-		return GetAsyncKeyState(VK_CONTROL) & 0x8000;
+  // Control
+  if (modifier == 2)
+    return GetAsyncKeyState(VK_CONTROL) & 0x8000;
 
-	// Alt
-	if (modifier == 3)
-		return GetAsyncKeyState(VK_MENU) & 0x8000;
+  // Alt
+  if (modifier == 3)
+    return GetAsyncKeyState(VK_MENU) & 0x8000;
 
-	// Control and Alt
-	if (modifier == 4)
-		return GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
+  // Control and Alt
+  if (modifier == 4)
+    return GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
 
-	// Shift
-	if (modifier == 5)
-		return GetAsyncKeyState(VK_SHIFT) & 0x8000;
+  // Shift
+  if (modifier == 5)
+    return GetAsyncKeyState(VK_SHIFT) & 0x8000;
 
-	// Shift and Alt
-	if (modifier == 6)
-		return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
+  // Shift and Alt
+  if (modifier == 6)
+    return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
 
-	// Shift and Control
-	if (modifier == 7)
-		return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_CONTROL) & 0x8000;
+  // Shift and Control
+  if (modifier == 7)
+    return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_CONTROL) & 0x8000;
 
-	// Shift and Control and Alt
-	if (modifier == 8)
-		return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
+  // Shift and Control and Alt
+  if (modifier == 8)
+    return GetAsyncKeyState(VK_SHIFT) & 0x8000 && GetAsyncKeyState(VK_CONTROL) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000;
 
-	return true;
+  return true;
 };
 
 bool StickyKeyboardInCHOP::getKeyPressed(string key)
 {
-	// active flag test
-	if (!active)
-		return false;
+  // active flag test
+  if (!active)
+    return false;
 
-	// modifier key test
-	if (!testModifierKey())
-		return false;
+  // modifier key test
+  if (!testModifierKey())
+    return false;
 
-	if (key == "comma")
-		return GetAsyncKeyState(VK_OEM_COMMA) & 0x8000;
+  if (key == "comma")
+    return GetAsyncKeyState(VK_OEM_COMMA) & 0x8000;
 
-	if (key == "period")
-		return GetAsyncKeyState(VK_OEM_PERIOD) & 0x8000;
+  if (key == "period")
+    return GetAsyncKeyState(VK_OEM_PERIOD) & 0x8000;
 
-	int code = (int)key[0];
+  int code = (int)key[0];
 
-	// 0 to 9
-	if (code >= 48 && code <= 57)
-	{
-		return GetAsyncKeyState(code) & 0x8000;
-	}
+  // 0 to 9
+  if (code >= 48 && code <= 57)
+  {
+    return GetAsyncKeyState(code) & 0x8000;
+  }
 
-	// a to z
-	if (code >= 97 && code <= 122)
-	{
-		return GetAsyncKeyState(code - 32) & 0x8000;
-	}
+  // a to z
+  if (code >= 97 && code <= 122)
+  {
+    return GetAsyncKeyState(code - 32) & 0x8000;
+  }
 
-	return false;
+  return false;
 }
 
 extern "C"
 {
-	DLLEXPORT void FillCHOPPluginInfo(CHOP_PluginInfo *info)
-	{
-		info->apiVersion = CHOPCPlusPlusAPIVersion;
-		info->customOPInfo.opType->setString("Stickykeyboardin");
-		info->customOPInfo.opLabel->setString("Sticky Keyboard In");
-		info->customOPInfo.authorName->setString("Akira Kamikura");
-		info->customOPInfo.authorEmail->setString("akira.kamikura@gmail.com");
+  DLLEXPORT void FillCHOPPluginInfo(CHOP_PluginInfo *info)
+  {
+    info->apiVersion = CHOPCPlusPlusAPIVersion;
+    info->customOPInfo.opType->setString("Stickykeyboardin");
+    info->customOPInfo.opLabel->setString("Sticky Keyboard In");
+    info->customOPInfo.authorName->setString("Akira Kamikura");
+    info->customOPInfo.authorEmail->setString("akira.kamikura@gmail.com");
 
-		info->customOPInfo.opIcon->setString("SKI");
+    info->customOPInfo.opIcon->setString("SKI");
 
-		info->customOPInfo.minInputs = 0;
-		info->customOPInfo.maxInputs = 0;
-	}
+    info->customOPInfo.minInputs = 0;
+    info->customOPInfo.maxInputs = 0;
+  }
 
-	DLLEXPORT CHOP_CPlusPlusBase *CreateCHOPInstance(const OP_NodeInfo *info)
-	{
-		return new StickyKeyboardInCHOP(info);
-	}
+  DLLEXPORT CHOP_CPlusPlusBase *CreateCHOPInstance(const OP_NodeInfo *info)
+  {
+    return new StickyKeyboardInCHOP(info);
+  }
 
-	DLLEXPORT void DestroyCHOPInstance(CHOP_CPlusPlusBase *instance)
-	{
-		delete (StickyKeyboardInCHOP *)instance;
-	}
+  DLLEXPORT void DestroyCHOPInstance(CHOP_CPlusPlusBase *instance)
+  {
+    delete (StickyKeyboardInCHOP *)instance;
+  }
 };
